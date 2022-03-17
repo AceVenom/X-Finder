e519cf97c8eced8b0a0523fec517495828f2aa0b
AceVenom <64217969+AceVenom@users.noreply.github.com>
2022-03-17 11:01:00 +0100

    X-Finder

---
+++ X-Finder.py
@@ -0,0 +1,43 @@
+import time
+print(""" (`-')                             _     <-. (`-')_  _(`-')    (`-')  _   (`-')  
+ (OO )_.->                <-.     (_)       \( OO) )( (OO ).-> ( OO).-/<-.(OO )  
+ (_| \_)--.(`-')       (`-')-----.,-(`-'),--./ ,--/  \    .'_ (,------.,------,) 
+ \  `.'  / ( OO).->    (OO|(_\---'| ( OO)|   \ |  |  '`'-..__) |  .---'|   /`. ' 
+  \    .')(,------.     / |  '--. |  |  )|  . '|  |) |  |  ' |(|  '--. |  |_.' | 
+  .'    \  `------'     \_)  .--'(|  |_/ |  |\    |  |  |  / : |  .--' |  .   .' 
+ /  .'.  \               `|  |_)  |  |'->|  | \   |  |  '-'  / |  `---.|  |\  \  
+`--'   '--'               `--'    `--'   `--'  `--'  `------'  `------'`--' '--' """)
+print(""" _________________
+| author:         |
+| Ace-Venom       |
+|_________________|""")
+username = input("insert username: ")
+print("search " + " [" + username + "] " + " on")
+instagram = ("www.instagram.com/" + username + "/")
+time.sleep(10)
+print("instagram:  " + instagram)
+time.sleep(10)
+facebook = ("www.facebook.com/" + username + "/")
+print("facebook:  " + facebook)
+time.sleep(10)
+spotify = ("www.spotify.com/user/" + username + "/")
+time.sleep(10)
+print("spotify: " + spotify)
+time.sleep(10)
+tiktok =("www.tiktok.com/" + username + "/")
+print("tiktok: " + tiktok)
+time.sleep(10)
+telegram = ("t.me/" + username)
+print("Telegram: " + telegram)
+time.sleep(10)
+twitter = ("www.twitter.com/" + username + "/")
+print("twitter: " + twitter)
+time.sleep(10)
+reddit =("www.reddit.com/user/" + username +"/")
+print("Reddit: " + reddit)
+time.sleep(10)
+twitch = ("www.twitch.com/" + username + "/")
+print("Twitch: " + twitch)
+time.sleep(10)
+youtube = ("www.youtube.com/" + username +"/")
+print("Youtube: " + youtube)
