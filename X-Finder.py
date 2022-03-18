7c6f5f71e2e82935fd4d4104f639ddf9ec0bb420
AceVenom <64217969+AceVenom@users.noreply.github.com>
2022-03-18 12:55:40 +0100

    Update

--- X-Finder.py
+++ X-Finder.py
@@ -1,12 +1,31 @@
 import time
-print(""" (`-')                             _     <-. (`-')_  _(`-')    (`-')  _   (`-')  
- (OO )_.->                <-.     (_)       \( OO) )( (OO ).-> ( OO).-/<-.(OO )  
- (_| \_)--.(`-')       (`-')-----.,-(`-'),--./ ,--/  \    .'_ (,------.,------,) 
- \  `.'  / ( OO).->    (OO|(_\---'| ( OO)|   \ |  |  '`'-..__) |  .---'|   /`. ' 
-  \    .')(,------.     / |  '--. |  |  )|  . '|  |) |  |  ' |(|  '--. |  |_.' | 
-  .'    \  `------'     \_)  .--'(|  |_/ |  |\    |  |  |  / : |  .--' |  .   .' 
- /  .'.  \               `|  |_)  |  |'->|  | \   |  |  '-'  / |  `---.|  |\  \  
-`--'   '--'               `--'    `--'   `--'  `--'  `------'  `------'`--' '--' """)
+print("""
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@&####BGBB#&@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@#&&5!YYP&BYJ7^~7YG&@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@#Y7:!~^J#@@@@@@&G7..:!?G&@@@@@@@@@@@@
+@@@@@@@@@@@#J^.....?BB#&@@@@&&GJ^   .7B@@@@@@@@@@@
+@@@@@@@@@@#7. . :.^P#&&G@&@BB###?::..!P&@@@@@@@@@@
+@@@@@@@@@@BG~   ..:Y#&#7J#B!5&#P!...:G#7P@@@@@@@@@
+@@@@@@@@@@#P^.:J?JP?JB#5G&&GP&G!5YY57J7:7#@@@@@@@@ X-Finder
+@@@@@@@@#7^:..^7!JG!.!5Y#@@BYG7~J7JG~...:P@@@@@@@@ by
+@@@@@@@@P. ... :Y7~..  ?&@@&JYG7:G5J!^^:^P@@@@@@@@ AceVenom
+@@@@@@@&B: ..  ~7   .~?G@@@@P!~:?Y?GBGB##&@@@@@@@@
+@@@@@@@@&J....:^^.!JG&@@@@@@@@BY5J.J&@@@@@@@@@@@@@
+@@@@@@@@@&?^7YP~^G#@@@@@@@@@@@@@&B!.?B#&@@@@@@@@@@
+@@@@@@@@@@@&&&?.5@@@@@@@@@@@@@@@@&5  :?#@@@@@@@@@@
+@@@@@@@@@@@@@#^P@@@@@@@@@@@@@@@@@@B^~5&@@@@@@@@@@@
+@@@@@@@@@@@@@@&@@@@@@@@@@@@@@@@@@@@&&@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+""")
+print("---------------(social-links)---------------------")
 print(""" _________________
 | author:         |
 | Ace-Venom       |
@@ -14,30 +33,33 @@ print(""" _________________
 username = input("insert username: ")
 print("search " + " [" + username + "] " + " on")
 instagram = ("www.instagram.com/" + username + "/")
-time.sleep(10)
+time.sleep(2)
 print("instagram:  " + instagram)
-time.sleep(10)
+time.sleep(2)
 facebook = ("www.facebook.com/" + username + "/")
 print("facebook:  " + facebook)
-time.sleep(10)
+time.sleep(2)
 spotify = ("www.spotify.com/user/" + username + "/")
-time.sleep(10)
+time.sleep(2)
 print("spotify: " + spotify)
-time.sleep(10)
+time.sleep(2)
 tiktok =("www.tiktok.com/" + username + "/")
 print("tiktok: " + tiktok)
-time.sleep(10)
+time.sleep(2)
 telegram = ("t.me/" + username)
 print("Telegram: " + telegram)
-time.sleep(10)
+time.sleep(2)
 twitter = ("www.twitter.com/" + username + "/")
 print("twitter: " + twitter)
-time.sleep(10)
+time.sleep(2)
 reddit =("www.reddit.com/user/" + username +"/")
 print("Reddit: " + reddit)
-time.sleep(10)
+time.sleep(2)
 twitch = ("www.twitch.com/" + username + "/")
 print("Twitch: " + twitch)
-time.sleep(10)
+time.sleep(2)
 youtube = ("www.youtube.com/" + username +"/")
 print("Youtube: " + youtube)
+time.sleep(2)
+tumblr = ("www.tumbl.com/" + username +"/")
+print("tumblr: " + tumblr)
